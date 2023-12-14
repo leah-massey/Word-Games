a0aec0b9e3da399fe029d21c122dc4770e31c654
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-14 17:44:50 +0000

    testSolution function added

--- src/main/kotlin/UnscrambleGame.kt
+++ src/main/kotlin/UnscrambleGame.kt
@@ -1,13 +1,14 @@
+
+// select randomWord from list
+// scramble the word
+// receive a guessedWord
+// compare guessedWord with the randomWord
+// if correct, congratulate and exit program
+// if incorrect show randomWord and exit program
+
 class UnscrambleGame {
 
-    // select randomWord from list
-    // scramble the word
-    // receive a guessedWord
-    // compare guessedWord with the randomWord
-    // if correct, congratulate and exit program
-    // if incorrect show randomWord and exit program
-
-    var randomWord: String = "tests"
+    var randomWord: String = ""
 
     fun selectRandomWord(wordList: List<String> = listOf("pound", "trice", "hired", "comma", "logic")): String {
         val randomIndex: Int = (0 .. (wordList.size -1)).shuffled().first()
@@ -16,18 +17,18 @@ class UnscrambleGame {
     }
 
     fun scramble(): String {
-        val randomOrder = (1..5).shuffled() // interesting - it's not the index but the position
+        val randomOrder = (1..5).shuffled() // interesting - it's not the index but the position when applied to a string
         val scrambledWord: String = randomOrder.map {
              randomWord.split("")[it]
         }.joinToString("")
-
         return scrambledWord
     }
 
     fun testSolution(solution: String): String {
-        return TODO()
+        if (solution == randomWord) {
+            return "Congrats, you got it! 🥳"
+        } else {
+            return "Incorrect! Better luck next time old friend"
+        }
     }
-
-
-
 }
