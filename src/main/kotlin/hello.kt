347e3d3911fadc2b1127673e5d513ff22075c520
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-14 17:39:48 +0000

    main function updated to make game play out in terminal

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -1,3 +1,7 @@
 fun main() {
-    println("Hello, World!")
+    val unscrambleGame = UnscrambleGame()
+    unscrambleGame.selectRandomWord()
+    println("Unscramble this word: ${unscrambleGame.scramble()}")
+    val solution = readLine()!!
+    println(unscrambleGame.testSolution(solution))
 }
