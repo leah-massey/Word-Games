19b8660e15c2c56bfdda323bf6d58d280f0fe5ac
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-14 16:41:18 +0000

    Test(randomWord): refactor

--- src/main/kotlin/UnscrambleGame.kt
+++ src/main/kotlin/UnscrambleGame.kt
@@ -1,4 +1,4 @@
-class Unscramble {
+class UnscrambleGame {
 
     // select randomWord from list
     // scramble the word
@@ -7,9 +7,12 @@ class Unscramble {
     // if correct, congratulate and exit program
     // if incorrect show randomWord and exit program
 
+    var randomWord: String? = null
 
-    fun randomWord(wordList: List<String> = listOf("pound", "trice", "hired", "comma", "logic")): String {
+    fun selectRandomWord(wordList: List<String> = listOf("pound", "trice", "hired", "comma", "logic")): String {
         val randomIndex: Int = (0 .. (wordList.size -1)).shuffled().first()
+        randomWord = wordList[randomIndex]
+        println(randomWord)
         return wordList[randomIndex]
     }
 
--- src/test/kotlin/UnscrambleTest.kt
+++ src/test/kotlin/UnscrambleTest.kt
@@ -1,14 +1,13 @@
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class UnscrambleTest {
 
     @Test
     fun `a word is randomly selected from the list provided as argument`() {
-        val unscramble = Unscramble()
+        val unscrambleGame = UnscrambleGame()
         val wordList: List<String> = listOf("pound", "trice", "hired", "comma", "logic")
-        val word = unscramble.randomWord()
+        val word = unscrambleGame.selectRandomWord()
 
         val isInList: Boolean = wordList.contains(word)
 
