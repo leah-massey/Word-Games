de56fd72b8af72614744bff5e6170b000f814c25
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-20 17:12:01 +0000

    Refactor: scramble()

--- src/main/kotlin/games/UnscrambleGame.kt
+++ src/main/kotlin/games/UnscrambleGame.kt
@@ -25,25 +25,16 @@ class UnscrambleGame(val wordProcessor: WordProcessor) {
 
     fun selectRandomWord(): String {
 
-        val randomIndex: Int = (0 .. (wordList.size -1)).shuffled().first()
+        val randomIndex: Int = (0..(wordList.size - 1)).shuffled().first()
         randomWord = wordList[randomIndex]
         return randomWord
     }
 
     fun scramble(): String {
-        // create a random order to apply to the letters of randomWord
-        var randomOrder: List<Int> = (1..5).shuffled() // interesting - it's not the index but the position when applied to a string
-        // keep generating random order until it differs from original order
-        while (randomOrder == (1..5).toList()) {
-            randomOrder = (1..5).shuffled()
+        scrambledRandomWord = randomWord.split("").shuffled().joinToString("")
+        if (scrambledRandomWord == randomWord) {
+           scramble()
         }
-        //apply random order to randomWord
-        scrambledRandomWord = randomOrder.map {
-            randomWord.split("")[it]
-
-        }.joinToString("")
-
-
         return scrambledRandomWord
     }
 
