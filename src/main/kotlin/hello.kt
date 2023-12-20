922d2b518db729fb09e99557fac94dead4907e80
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-20 17:20:18 +0000

    Refactor: UX improved

--- src/main/kotlin/hello.kt
+++ src/main/kotlin/hello.kt
@@ -4,8 +4,27 @@ import wordSource.WordProcessor
 fun main() {
     val wordProcessor = WordProcessor()
     val unscrambleGame = UnscrambleGame(wordProcessor)
-    println("Welcome to Word Games!")
-    println("Unscramble this word: ${unscrambleGame.generateAndScramble()}")
-    val solution = readLine()!!
-    println(unscrambleGame.checkSolution(solution))
+    println()
+    println("Welcome to Word Games 👋🏻")
+    println()
+    println("Scrambled word: ${unscrambleGame.generateAndScramble()}")
+    print("Type unscrambled guess and press ⮐ : ")
+    val userInput = readln()
+
+    if (unscrambleGame.checkSolutionCorrect(userInput)) {
+        println("Congrats, you got it! 🥳")
+    } else {
+        println("Nope! The correct answer was ${unscrambleGame.randomWord}.")
+    }
+    println()
 }
+
+// 👇🏻 Code experiment, trying to identify arrow keys 👇🏻
+//val userInput = System.console().readLine()
+//if (userInput?.toByteArray()?.contentEquals(byteArrayOf(27, 91, 67)) == true) {
+//    println("right arrow key pressed")
+//} else if (userInput?.toByteArray()?.contentEquals(byteArrayOf(27, 91, 68)) == true) {
+//    println("Left arrow key pressed")
+//} else {
+//    println("some other key pressed")
+//}
