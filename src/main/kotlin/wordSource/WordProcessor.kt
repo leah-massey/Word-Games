4fef1ac452aff8d42b76bd4a6f595b3d5633bcd1
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-12-22 13:04:26 +0000

    Refactor: path name for words.txt file updated (previously link would only work on my computer)

--- src/main/kotlin/wordSource/WordProcessor.kt
+++ src/main/kotlin/wordSource/WordProcessor.kt
@@ -3,7 +3,7 @@ import java.io.File
 
 class WordProcessor() {
 
-    val filePath = "/Users/leahmassey/Documents/Kotlin/Word_Games/src/main/kotlin/wordSource/words.txt"
+    val filePath = javaClass.classLoader.getResource("words.txt")?.file
     val wordsAsList: List<String> =File(filePath).readLines()
     fun findFiveLetterWords(): List<String>{
         return wordsAsList.filter { it.length == 5}
